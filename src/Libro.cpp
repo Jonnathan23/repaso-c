9af590af25d000537d802e7c32a158976ce99774
Jonnathan23 <jsaquicela26@gmail.com>
2026-05-06 22:35:02 -0500

    encapsulamiento

--- src/Libro.cpp
+++ src/Libro.cpp
@@ -5,6 +5,10 @@
 using namespace std;
 
 class Libro {
+
+  private:
+    double precio;
+
   public:
     string titulo;
     string autor;
@@ -17,15 +21,32 @@ class Libro {
     ~Libro() {
         cout << "Destructuor llamando para: " << this->titulo << endl;
     }
+
+    void ponerPrecio(double nuevoPrecio) {
+        if (nuevoPrecio <= 0) {
+            cout << "Precio invalido" << endl;
+            return;
+        }
+
+        this->precio = nuevoPrecio;
+    }
+
+    double obtenerPrecio() {
+        return this->precio;
+    }
 };
 
 int main(int argc, char *argv[]) {
 
     //* Llaves para indicar el ciclo de vida del objeto
     {
-        Libro persona("20 mil leguajes de viaje submarino","Julio Verne");
+        Libro libro("20 mil leguajes de viaje submarino", "Julio Verne");
 
         cout << "Dentro del bloque main con el libro construido" << endl;
+
+        libro.ponerPrecio(26);
+
+        cout << "El nuevo precio del libro es: " << libro.obtenerPrecio() << endl;
     }
 
     cout << " \nFin del programa :)";
